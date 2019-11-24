acfe8dc00c417047bc8993ae6e322cca2028e8d1
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2019-11-24 19:24:44 +0200

    [WIP] Save secret to db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import React, { useState } from 'react'
+import fetch from 'isomorphic-unfetch'
+import { useSnackbar } from 'notistack'
+
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { makeStyles } from '@material-ui/core/styles'
+
+import SecretSettings from '../components/SecretSettings'
+import SecretLink from '../components/SecretLink'
+
+// TODO: add babel improt plugin
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -27,7 +37,6 @@ const useStyles = makeStyles(theme => ({
   },
   unencryptedSecret: {
     margin: 0,
-    marginBottom: theme.spacing(2),
     [theme.breakpoints.down('xs')]: {
       flex: 1,
       height: '100%',
@@ -38,28 +47,126 @@ const useStyles = makeStyles(theme => ({
       height: '100%',
     },
   },
+  secretWrapper: {
+    [theme.breakpoints.down('xs')]: {
+      height: '100%',
+    },
+    position: 'relative',
+  },
+  secretSettings: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    zIndex: theme.zIndex.speedDial,
+    fontSize: '1.2rem',
+  },
+  submitWrapper: {
+    position: 'relative',
+    marginTop: theme.spacing(2),
+  },
+  submitButton: {
+    width: '100%',
+  },
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
 }))
 
+const initialSecretData = {
+  secret: '',
+}
+
+interface Props {
+  uuid: string
+}
+
 const Home = () => {
+  const [secretData, setSecretData] = useState(initialSecretData)
+  const [submitting, setSubmitting] = useState(false)
+  const [secretLink, setSecretLink] = useState<string | null>(null)
   const classes = useStyles()
+  const { enqueueSnackbar } = useSnackbar()
+
+  const submit = async () => {
+    if (submitting) {
+      return
+    }
+
+    setSubmitting(true)
+
+    const encryptedSecret = secretData.secret
+
+    try {
+      const response = await fetch('/api/secrets', {
+        method: 'POST',
+        body: JSON.stringify({
+          encrypted_secret: encryptedSecret,
+        }),
+      })
+
+      const data = await response.json()
+
+      setSecretLink(`${window.location.origin}/${data.uuid}#TODO`)
+    } catch {
+      enqueueSnackbar('Failed to save your secret. Please try again later...', {
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+      })
+    }
+
+    setSubmitting(false)
+  }
 
   return (
     <Box className={classes.root}>
       <Container maxWidth="sm" className={classes.container}>
-        <TextField
-          className={classes.unencryptedSecret}
-          label="Tell me a secret..."
-          multiline
-          rows="8"
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          InputProps={{ className: classes.unencryptedSecretInput }}
-          inputProps={{ style: { height: '100%' } }}
-        />
+        <div className={classes.secretWrapper}>
+          <SecretSettings className={classes.secretSettings} />
 
-        <Button color="secondary">{'Share it!'}</Button>
+          <TextField
+            className={classes.unencryptedSecret}
+            label="Tell me a secret..."
+            multiline
+            value={secretData.secret}
+            rows="8"
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            InputProps={{ className: classes.unencryptedSecretInput }}
+            inputProps={{ style: { height: '100%' } }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSecretData({ ...secretData, secret: event.target.value })
+            }
+          />
+        </div>
+
+        <div className={classes.submitWrapper}>
+          <Button
+            className={classes.submitButton}
+            color="secondary"
+            onClick={submit}
+            disabled={submitting}
+          >
+            {'Share it!'}
+          </Button>
+
+          {submitting && (
+            <CircularProgress
+              color="secondary"
+              size={24}
+              className={classes.buttonProgress}
+            />
+          )}
+        </div>
       </Container>
+
+      <SecretLink link={secretLink} onClose={() => setSecretLink(null)} />
     </Box>
   )
 }
