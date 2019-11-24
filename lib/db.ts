3cc2d8b1a4d4ac6ec4f2918f0c4fe597edcb9b86
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2019-11-24 19:20:31 +0200

    Add database

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+import { promisify } from 'util'
+import redis from 'redis'
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: parseInt(process.env.REDIS_PORT || '6379'),
+  url: process.env.REDIS_URL,
+})
+
+client.on('connect', function() {
+  console.log('Redis client connected')
+})
+
+client.on('error', function(err) {
+  console.log('Something went wrong ' + err)
+})
+
+export default {
+  set: promisify(client.set).bind(client),
+  get: promisify(client.get).bind(client),
+}
